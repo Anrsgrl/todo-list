f54727b0ec4f721f1a1255d3747ab61357f54a5c
Anrsgrl <esgerlianar003@gmail.com>
2023-03-08 22:35:59 +0300

    filter added/ background changed/ functionality updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,40 @@
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { purple } from '@mui/material/colors';
 import React from 'react';
 import './App.scss';
 import Todo from './components/Todo/Todo';
 
 function App() {
   let theme = createTheme({
+    palette: {
+      primary: {
+        main: purple[300],
+        dark: purple[500],
+      },
+      white: {
+        main: '#fff'
+      },
+      secondary: {
+        main: 'rgb(16, 16, 16)',
+      },
+    },
     typography: {
-      h5:{
+      h5: {
         color: 'white',
         textTransform: 'uppercase',
-        fontWeight: 'bolder', 
+        fontWeight: 'bolder',
         lineHeight: '2',
         letterSpacing: '0.5em',
       }
-    }
+    },
+    textField: {
+      fontSize: '35px',
+      fontFamily: [
+        'Poppins',
+        'sans-serif',
+      ].join(','),
+    },
+    border: '1px solid blue',
   })
 
   return (
